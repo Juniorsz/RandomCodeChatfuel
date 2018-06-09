e1ec206643cbca6286cc81d88dddf15f91fc90b5
Junior <35627119+Juniorsz@users.noreply.github.com>
2018-06-09 22:42:14 +0700

    Update account.sql

--- account.sql
+++ account.sql
@@ -2,8 +2,8 @@
 -- version 3.5.8.2
 -- http://www.phpmyadmin.net
 --
--- Host: sql300.byetcluster.com
--- Generation Time: Jun 09, 2018 at 03:12 AM
+-- Host: sql300.byethost.com
+-- Generation Time: Jun 09, 2018 at 11:42 AM
 -- Server version: 5.6.35-81.0
 -- PHP Version: 5.3.3
 
@@ -28,22 +28,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `account` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `username` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `password` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `code` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `status` int(11) NOT NULL,
   `uid` int(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`id`, `username`, `password`, `status`, `uid`) VALUES
-(1, 'test1', '123456', 1, 0),
-(2, 'test2', '654321', 1, 0),
-(3, 'test3', 'hello', 1, 0),
-(4, 'test4', 'goodbye', 1, 0);
+INSERT INTO `account` (`id`, `code`, `status`, `uid`) VALUES
+(2, 'test2', 1, 0),
+(9, 'king', 1, 0),
+(4, 'test4', 1, 0),
+(6, 'vydeptrai', 1, 0),
+(7, 'killer 1221121212', 1, 0),
+(8, 'vipcolor', 0, 0),
+(11, 'vip 121221', 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
